461e15f72002b65b3426407858277bb24b12b7d6
edwin caipa <edwin.caipara@cun.edu.co>
2023-08-10 10:04:10 -0500

    subiendo archivos

---
+++ src/js/colors.js
@@ -0,0 +1,18 @@
+const colors = {
+  // Main text
+  colorHeader: '#ffffff',
+  colorBody: '#000000',
+  colorFooter: '#545454',
+  // Accent colors
+  colorAccent: '#ff6b08',
+  colorAccentSec: '#ff6b01',
+  colorLinkUnderline: '#ff001b',
+  // Background
+  colorBcg: '#e5e5e5',
+  colorBcgFooter: '#f7f7f7',
+  // Input forms
+  colorTextPlaceholder: '#505050f',
+  colorTextInvalid: '#ff705d',
+};
+
+export default colors;
